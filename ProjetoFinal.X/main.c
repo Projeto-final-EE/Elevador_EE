870665d846b63cf660de282dfe3a76f286cfad52
Guilherme Costa <guilhermercosta1@gmail.com>
2023-07-19 16:58:25 -0300

    Duty

--- ProjetoFinal.X/main.c
+++ ProjetoFinal.X/main.c
@@ -209,12 +209,12 @@ void controleMovimento(){
             }
             break;
         case EmTrajeto:
-            PWM3_LoadDutyValue(DUTTY);
+            PWM3_LoadDutyValue(DUTY);
             TMR4_StopTimer();
             cont = 0;
             break;
         case RetornaS0:
-            PWM3_LoadDutyValue(DUTTY);
+            PWM3_LoadDutyValue(DUTY);
             cont = 0;
             TMR4_StopTimer();
             break;
